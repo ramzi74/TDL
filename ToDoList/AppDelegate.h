9bf1f6146cf064a713c14065ccf770360c42e89f
ramzi74 <ramzi74@gmail.com>
2018-01-16 11:24:51 -0800

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Ramzi on 01/12/2018.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Ramzi on 2/16/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
